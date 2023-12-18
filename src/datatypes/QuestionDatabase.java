8397c12dbda7276be3df1440d35a58c99633a53b
Jakubkkk12 <jakubkow111@gmail.com>
2023-12-18 01:54:07 +0100

    Update QuestionDatabase.java

--- src/datatypes/QuestionDatabase.java
+++ src/datatypes/QuestionDatabase.java
@@ -1,21 +1,32 @@
 package datatypes;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class QuestionDatabase {
 
-    private static ArrayList<QuestionModel> questions;
+    private ArrayList<QuestionModel> questions;
     
     public QuestionDatabase() {
-		questions = new ArrayList<QuestionModel>();
+		questions = new ArrayList<QuestionModel>(Arrays.asList(null,null,null,null,null,null,null,null,null,null,null,null,null));
 	}
     
-    public void addQuestionModel(QuestionModel questionModel) {
-    	questions.add(questionModel);
+    public void setQuestionModel(int index, QuestionModel questionModel) {
+    	questions.set(index, questionModel);
 	}
 
     public QuestionModel getQuestionModel(int index) {
         return questions.get(index);
     }
 
-   
+    public int getNullIndex() {
+		for (int i = 0; i < questions.size(); i++) {
+			if (questions.get(i) == null) return i;
+			if (questions.get(i).isNull()) return i;
+		} 			
+    	return -1;
+	}
+
+    public int size() {
+		return 13;
+	}
 }
